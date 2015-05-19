db02789ef0fd453841b92845b47a34cf7c9c73a1
Dan Richardson <dan.richardson.gs@gmail.com>
2015-05-19 12:26:11 +0100

    Sending feedback averages in app-parsable objects

--- Crowd.Service/Controller/FeedbackController.cs
+++ Crowd.Service/Controller/FeedbackController.cs
@@ -1,6 +1,12 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Crowd.Model.Data;
+using Crowd.Service.Interface;
+using Crowd.Service.Model.Feedback;
 using Newtonsoft.Json;
 
 namespace Crowd.Service.Controller
@@ -9,10 +15,42 @@ namespace Crowd.Service.Controller
     {
         public async Task<HttpResponseMessage> Get()
         {
-            var acts = await DB.CrowdJudgements.ToListAsync();
+            // get average of all accent ratings
+            float rlstaccent = (float) await (from judgement in DB.CrowdJudgements
+                from data in judgement.Data
+                where data.DataType == "rlstaccent"
+                select data.NumResponse).AverageAsync();
+
+            FeedbackItemRating accentFeedback = new FeedbackItemRating
+            {
+                Rating = rlstaccent,
+                Title = "Accent Influence",
+                Description = "This rating shows how much your accent affected listeners' understanding of your speech.",
+                Date = DateTime.Now,
+                Dismissable = false,
+                Importance = 10
+            };
+
+            float rlsttrans = (float)await (from judgement in DB.CrowdJudgements
+                                             from data in judgement.Data
+                                            where data.DataType == "rlsttrans"
+                                             select data.NumResponse).AverageAsync();
+
+            FeedbackItemRating transFeedback = new FeedbackItemRating
+            {
+                Rating = rlsttrans,
+                Title = "Difficulty of Understanding",
+                Description = "This rating shows how difficult listeners find understanding what you say.",
+                Date = DateTime.Now,
+                Dismissable = false,
+                Importance = 10
+            };
+
+            IFeedbackItem[] feedback = {transFeedback, accentFeedback};
+
             return new HttpResponseMessage
             {
-                Content = new JsonContent(JsonConvert.SerializeObject(acts))
+                Content = new JsonContent(JsonConvert.SerializeObject(feedback))
             };
         }
     }
---
+++ Crowd.Service/Interface/IFeedbackItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Crowd.Service.Interface
+{
+    public interface IFeedbackItem
+    {
+        int Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        DateTime Date { get; set; }
+        bool Dismissable { get; set; }
+        int Importance { get; set; }
+    }
+}
