4417161b3b9699772659ba9345ecc335b9c95eac
Dan Richardson <dan.richardson.gs@gmail.com>
2015-06-08 11:19:07 +0100

    Success with webhook

--- Crowd.Service/Controller/CFWebhookController.cs
+++ Crowd.Service/Controller/CFWebhookController.cs
@@ -33,63 +33,71 @@ namespace Crowd.Service.Controller
             return Request.CreateResponse(HttpStatusCode.OK);
         }
 
+        // Should always return OK - crowdflower can't really do anything with errors. Log any errors in the database.
         public async Task<HttpResponseMessage> Post()
         {
-            // TEMP for local testing TODO
             var req = this.Request;
-            CFWebhook realData = JsonConvert.DeserializeObject<CFWebhook>(await req.Content.ReadAsStringAsync());
-            //string realData = (await req.Content.ReadAsFormDataAsync())["payload"];
-
-            //const string jsonData = "{\r\n    \"signature\": \"645d58c9e38fc4d7b00f215ce4a72b218cc4b52b\",\r\n    \"payload\": \"{\\\"id\\\":698113669,\\\"data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"},\\\"difficulty\\\":0,\\\"judgments_count\\\":3,\\\"state\\\":\\\"finalized\\\",\\\"agreement\\\":0.444444444444444,\\\"missed_count\\\":0,\\\"gold_pool\\\":null,\\\"created_at\\\":\\\"2015-04-02T15:13:54+00:00\\\",\\\"updated_at\\\":\\\"2015-04-02T15:54:26+00:00\\\",\\\"job_id\\\":710069,\\\"results\\\":{\\\"judgments\\\":[{\\\"id\\\":1604431321,\\\"created_at\\\":\\\"2015-04-02T15:17:33+00:00\\\",\\\"started_at\\\":\\\"2015-04-02T15:16:14+00:00\\\",\\\"acknowledged_at\\\":null,\\\"external_type\\\":\\\"clixsense\\\",\\\"golden\\\":false,\\\"missed\\\":null,\\\"rejected\\\":null,\\\"tainted\\\":false,\\\"country\\\":\\\"TUR\\\",\\\"region\\\":\\\"34\\\",\\\"city\\\":\\\"Istanbul\\\",\\\"job_id\\\":710069,\\\"unit_id\\\":698113669,\\\"worker_id\\\":29230631,\\\"trust\\\":1.0,\\\"worker_trust\\\":0.915,\\\"unit_state\\\":\\\"finalized\\\",\\\"data\\\":{\\\"txta\\\":\\\"hello, can i order a pizza please?\\\",\\\"rlsttrans\\\":\\\"3\\\",\\\"rlstaccent\\\":\\\"3\\\"},\\\"unit_data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"}},{\\\"id\\\":1604431382,\\\"created_at\\\":\\\"2015-04-02T15:18:02+00:00\\\",\\\"started_at\\\":\\\"2015-04-02T15:16:14+00:00\\\",\\\"acknowledged_at\\\":null,\\\"external_type\\\":\\\"clixsense\\\",\\\"golden\\\":false,\\\"missed\\\":null,\\\"rejected\\\":null,\\\"tainted\\\":false,\\\"country\\\":\\\"PRT\\\",\\\"region\\\":\\\"\\\",\\\"city\\\":\\\"\\\",\\\"job_id\\\":710069,\\\"unit_id\\\":698113669,\\\"worker_id\\\":23097572,\\\"trust\\\":0.5,\\\"worker_trust\\\":0.923636363636364,\\\"unit_state\\\":\\\"finalized\\\",\\\"data\\\":{\\\"txta\\\":\\\"Hello, can i order a pizza please?\\\",\\\"rlsttrans\\\":\\\"5 Very easy\\\",\\\"rlstaccent\\\":\\\"1 Not at all\\\"},\\\"unit_data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"}},{\\\"id\\\":1604449343,\\\"created_at\\\":\\\"2015-04-02T15:54:15+00:00\\\",\\\"started_at\\\":\\\"2015-04-02T15:46:15+00:00\\\",\\\"acknowledged_at\\\":null,\\\"external_type\\\":\\\"neodev\\\",\\\"golden\\\":false,\\\"missed\\\":null,\\\"rejected\\\":null,\\\"tainted\\\":false,\\\"country\\\":\\\"VEN\\\",\\\"region\\\":\\\"\\\",\\\"city\\\":\\\"\\\",\\\"job_id\\\":710069,\\\"unit_id\\\":698113669,\\\"worker_id\\\":32002644,\\\"trust\\\":0.5,\\\"worker_trust\\\":0.9,\\\"unit_state\\\":\\\"finalized\\\",\\\"data\\\":{\\\"txta\\\":\\\"Hello can i order a pizza please?\\\",\\\"rlsttrans\\\":\\\"5 Very easy\\\",\\\"rlstaccent\\\":\\\"5 Very much\\\"},\\\"unit_data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"}}],\\\"rlsttrans\\\":{\\\"agg\\\":null,\\\"confidence\\\":0},\\\"rlstaccent\\\":{\\\"agg\\\":null,\\\"confidence\\\":0}}}\",\r\n    \"signal\": \"unit_complete\"\r\n}";
-
-            CFResponseData cfData = JsonConvert.DeserializeObject<CFResponseData>(realData.payload);
-
-            if (cfData == null || cfData.results == null || cfData.results.judgments.Count <= 0)
-                return Request.CreateResponse(HttpStatusCode.InternalServerError);
-
-            using (CrowdContext db = new CrowdContext())
+            string content =  (await req.Content.ReadAsFormDataAsync())["payload"];
+            try
             {
-                int taskId = cfData.results.judgments[0].unit_id;
-                CrowdRowResponse resp = await db.CrowdRowResponses.FindAsync(taskId.ToString());
+                //const string jsonData = "{\r\n    \"signature\": \"645d58c9e38fc4d7b00f215ce4a72b218cc4b52b\",\r\n    \"payload\": \"{\\\"id\\\":698113669,\\\"data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"},\\\"difficulty\\\":0,\\\"judgments_count\\\":3,\\\"state\\\":\\\"finalized\\\",\\\"agreement\\\":0.444444444444444,\\\"missed_count\\\":0,\\\"gold_pool\\\":null,\\\"created_at\\\":\\\"2015-04-02T15:13:54+00:00\\\",\\\"updated_at\\\":\\\"2015-04-02T15:54:26+00:00\\\",\\\"job_id\\\":710069,\\\"results\\\":{\\\"judgments\\\":[{\\\"id\\\":1604431321,\\\"created_at\\\":\\\"2015-04-02T15:17:33+00:00\\\",\\\"started_at\\\":\\\"2015-04-02T15:16:14+00:00\\\",\\\"acknowledged_at\\\":null,\\\"external_type\\\":\\\"clixsense\\\",\\\"golden\\\":false,\\\"missed\\\":null,\\\"rejected\\\":null,\\\"tainted\\\":false,\\\"country\\\":\\\"TUR\\\",\\\"region\\\":\\\"34\\\",\\\"city\\\":\\\"Istanbul\\\",\\\"job_id\\\":710069,\\\"unit_id\\\":698113669,\\\"worker_id\\\":29230631,\\\"trust\\\":1.0,\\\"worker_trust\\\":0.915,\\\"unit_state\\\":\\\"finalized\\\",\\\"data\\\":{\\\"txta\\\":\\\"hello, can i order a pizza please?\\\",\\\"rlsttrans\\\":\\\"3\\\",\\\"rlstaccent\\\":\\\"3\\\"},\\\"unit_data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"}},{\\\"id\\\":1604431382,\\\"created_at\\\":\\\"2015-04-02T15:18:02+00:00\\\",\\\"started_at\\\":\\\"2015-04-02T15:16:14+00:00\\\",\\\"acknowledged_at\\\":null,\\\"external_type\\\":\\\"clixsense\\\",\\\"golden\\\":false,\\\"missed\\\":null,\\\"rejected\\\":null,\\\"tainted\\\":false,\\\"country\\\":\\\"PRT\\\",\\\"region\\\":\\\"\\\",\\\"city\\\":\\\"\\\",\\\"job_id\\\":710069,\\\"unit_id\\\":698113669,\\\"worker_id\\\":23097572,\\\"trust\\\":0.5,\\\"worker_trust\\\":0.923636363636364,\\\"unit_state\\\":\\\"finalized\\\",\\\"data\\\":{\\\"txta\\\":\\\"Hello, can i order a pizza please?\\\",\\\"rlsttrans\\\":\\\"5 Very easy\\\",\\\"rlstaccent\\\":\\\"1 Not at all\\\"},\\\"unit_data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"}},{\\\"id\\\":1604449343,\\\"created_at\\\":\\\"2015-04-02T15:54:15+00:00\\\",\\\"started_at\\\":\\\"2015-04-02T15:46:15+00:00\\\",\\\"acknowledged_at\\\":null,\\\"external_type\\\":\\\"neodev\\\",\\\"golden\\\":false,\\\"missed\\\":null,\\\"rejected\\\":null,\\\"tainted\\\":false,\\\"country\\\":\\\"VEN\\\",\\\"region\\\":\\\"\\\",\\\"city\\\":\\\"\\\",\\\"job_id\\\":710069,\\\"unit_id\\\":698113669,\\\"worker_id\\\":32002644,\\\"trust\\\":0.5,\\\"worker_trust\\\":0.9,\\\"unit_state\\\":\\\"finalized\\\",\\\"data\\\":{\\\"txta\\\":\\\"Hello can i order a pizza please?\\\",\\\"rlsttrans\\\":\\\"5 Very easy\\\",\\\"rlstaccent\\\":\\\"5 Very much\\\"},\\\"unit_data\\\":{\\\"AudioUrl\\\":\\\"http://api.opescode.com/Uploads/710069/4.mp4\\\",\\\"AudioTypeCodec\\\":\\\"audio/mp4; codec='mp4a.40.2'\\\"}}],\\\"rlsttrans\\\":{\\\"agg\\\":null,\\\"confidence\\\":0},\\\"rlstaccent\\\":{\\\"agg\\\":null,\\\"confidence\\\":0}}}\",\r\n    \"signal\": \"unit_complete\"\r\n}";
 
-                if (resp.TaskJudgements == null) resp.TaskJudgements = new List<CrowdJudgement>();
+                CFResponseData cfData = JsonConvert.DeserializeObject<CFResponseData>(content);
 
-                foreach (Judgement judgement in cfData.results.judgments)
+                if (cfData == null || cfData.results == null || cfData.results.judgments.Count <= 0)
+                    return Request.CreateResponse(HttpStatusCode.OK);
+
+                using (CrowdContext db = new CrowdContext())
                 {
-                    if (resp.TaskJudgements.Any(judge =>
-                        judge.WorkerId == judgement.worker_id &&
-                        judge.CrowdRowResponseId == resp.Id
-                        )) continue;
+                    int taskId = cfData.results.judgments[0].unit_id;
+                    CrowdRowResponse resp = await db.CrowdRowResponses.FindAsync(taskId.ToString());
 
-                    CrowdJudgement cJudgement = new CrowdJudgement
+                    if (resp.TaskJudgements == null) resp.TaskJudgements = new List<CrowdJudgement>();
+
+                    foreach (Judgement judgement in cfData.results.judgments)
                     {
-                        Id = judgement.id,
-                        City = judgement.city,
-                        Country = judgement.country,
-                        JobId = judgement.job_id,
-                        CrowdRowResponseId = resp.Id,
-                        Tainted = judgement.tainted,
-                        Trust = judgement.trust,
-                        WorkerId = judgement.worker_id,
-                        CreatedAt = DateTime.Now
-                    };
-                    resp.TaskJudgements.Add(cJudgement);
-                    db.CrowdJudgements.Add(cJudgement);
+                        if (resp.TaskJudgements.Any(judge =>
+                            judge.WorkerId == judgement.worker_id &&
+                            judge.CrowdRowResponseId == resp.Id
+                            )) continue;
 
-                    await cJudgement.AddData(judgement.data, db);
+                        CrowdJudgement cJudgement = new CrowdJudgement
+                        {
+                            Id = judgement.id,
+                            City = judgement.city,
+                            Country = judgement.country,
+                            JobId = judgement.job_id,
+                            CrowdRowResponseId = resp.Id,
+                            Tainted = judgement.tainted,
+                            Trust = judgement.trust,
+                            WorkerId = judgement.worker_id,
+                            CreatedAt = DateTime.Now
+                        };
+                        resp.TaskJudgements.Add(cJudgement);
+                        db.CrowdJudgements.Add(cJudgement);
+
+                        await cJudgement.AddData(judgement.data, db);
+                    }
+
+                    db.SaveChanges();
+
+                    return Request.CreateResponse(HttpStatusCode.OK);
                 }
-
-                try
+            }
+            catch (Exception ex)
+            {
+                using (CrowdContext db = new CrowdContext())
                 {
+                    db.DebugMessages.Add(new DebugMessage
+                    {
+                        Message = ex + "\nSENT CONTENT: " + content,
+                        Filename = "CFWebhookController",
+                        FunctionName = "Post"
+                    });
                     db.SaveChanges();
                 }
-                catch (Exception ex)
-                {
-                    return Request.CreateResponse(HttpStatusCode.InternalServerError, ex);
-                }
-                return Request.CreateResponse(HttpStatusCode.OK);
+                return Request.CreateResponse(HttpStatusCode.OK); 
             }
-            
         }
 
         public HttpResponseMessage Delete(int id)
--- Crowd.Service/CrowdFlower/CFWebhook.cs
+++ Crowd.Service/CrowdFlower/CFWebhook.cs
@@ -1,9 +1,4 @@
-﻿using Crowd.Model.Data;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 
 namespace Crowd.Service.CrowdFlower
 {
